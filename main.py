7ff7a298ba9f6faeb54bc2b0014ebc00a2c26d7e
22ljz <2380189206@qq.com>
2026-04-23 08:40:50 +0000

    1

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import discord
 import json
 import os
 import asyncio
+import sys
+import time
 
-config = json.loads(os.environ['CONFIG'])
+config = json.loads(os.environ["CONFIG"])
 
 client = discord.Client()
 
@@ -13,14 +15,34 @@ client = discord.Client()
 async def on_ready():
     game = discord.Activity(
         type=discord.ActivityType.playing,
-        application_id=config['application_id'],
-        name=config['name'],
-        assets=config['assets'],
+        application_id=config["application_id"],
+        name=config["name"],
+        assets=config["assets"],
         start=datetime.now(),
     )
     await client.change_presence(activity=game, status=discord.Status.online)
-    await asyncio.sleep(4 * 60 * 60)
+
+    start = time.time()
+
+    for guild in client.guilds:
+        async for message in guild.search(
+            authors=[client.user],
+            before=datetime.now() - timedelta(days=7),
+            limit=sys.maxsize,
+        ):
+            await message.delete()
+
+    for user in client.users:
+        async for message in user.search(
+            authors=[client.user],
+            before=datetime.now() - timedelta(days=7),
+            limit=sys.maxsize,
+        ):
+            await message.delete()
+
+    await asyncio.sleep(4 * 60 * 60 - (time.time() - start))
     await client.close()
 
+
 if __name__ == "__main__":
-    client.run(os.environ['TOKEN'], log_handler=None)
+    client.run(os.environ["TOKEN"], log_handler=None)
